f3cd3f3c7bb40fe416851390f498f839eed6bb06
Lautaro Nievas <lautynievas09@gmail.com>
2023-12-29 23:07:23 -0300

    environment varialbes added

--- src/db.js
+++ src/db.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://nievas100:29075159@cluster0.ub7ny08.mongodb.net/?retryWrites=true&w=majority",
-      {
-        autoIndex: true,
-      }
-    );
+    await mongoose.connect(process.env.MONGO_URL, {
+      autoIndex: true,
+    });
     console.log("Successful connection to the database");
   } catch (error) {
     console.error("Error connecting to database:", error);
